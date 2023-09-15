7a85c53adf1941d200204dcf17d0d48ba393330d
Jaime Salebe <jaimesalebe@outlook.com>
2023-09-15 09:24:25 -0500

    api-rest-movies deploy in fl0

---
+++ app.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
+const cors = require('cors')
+const {
+  validateMovie,
+  validatePartialMovie
+} = require('./schemas/movies.schema')
+
+const app = express()
+app.use(express.json())
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://movies.com',
+        'https://midu.dev'
+      ]
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+
+      if (!origin) {
+        return callback(null, true)
+      }
+
+      return callback(new Error('Not allowed by CORS'))
+    }
+  })
+)
+
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to movies api 🎞️',
+    routes: {
+      movies: '/movies',
+      movieById: '/movies/dcdd0fad-a94c-4810-8acc-5f108d3b18c3',
+      movieByGenre: '/movies?genre=sci-fi'
+    }
+  })
+})
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(movie =>
+      movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // en base de datos
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+  return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 8080
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`)
+})
---
+++ schemas/movies.schema.js
@@ -0,0 +1,43 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be string',
+    required_error: 'Movie title is required'
+  }),
+  genre: z.array(
+    z.enum(
+      [
+        'Action',
+        'Adventure',
+        'Comedy',
+        'Drama',
+        'Fantasy',
+        'Horror',
+        'Thriller',
+        'Sci-Fi'
+      ],
+      {
+        required_error: 'Movie genre is required',
+        invalid_type_error: 'Movie genre must be an array of strings'
+      }
+    )
+  ),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  })
+})
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie (object) {3
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = { validateMovie, validatePartialMovie }
